91aca06dd568e05ccbdc5cf02486e0d9ed756028
Mohammad <fathim2000astronomy@gmail.com>
2019-07-01 15:23:53 -0800

    edit user class

--- University-resource-management/login.cpp
+++ University-resource-management/login.cpp
@@ -46,6 +46,9 @@ void Login::on_pb_getinfo_clicked()
     sign.Set_phonenum(ui->LE_phone->text());
     sign.Set_password(ui->LE_pass_2->text());
     sign.Set_username(ui->LE_user_2->text());
+    sign.Set_address(ui->LE_address->text());
+    sign.change_available("t");
+    sign.Set_office_ID("U");
 
     ui->stackedWidget->setCurrentIndex(0);
     //insert info to file.

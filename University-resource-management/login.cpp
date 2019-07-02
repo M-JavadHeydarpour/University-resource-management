2295fa2363e69b42933926656be914f4d4a613cd
Mohammad <fathim2000astronomy@gmail.com>
2019-07-02 15:36:45 -0800

    fixed bugs of person_database class

--- University-resource-management/login.cpp
+++ University-resource-management/login.cpp
@@ -103,6 +103,10 @@ void Login::on_PB_test_clicked()
     qDebug() <<t.Get_role();
     qDebug() <<t.Get_office_ID();
     */
-    qDebug()<<test.Search_Family("fathi");
+    //test.Delete("1003");
+    User t;
+    t=test.extarct_data("1002;bibbib;9632;habib;sharif;ibib@gmail.com;shahin shahr;09130078;#;U;#;");
+    t.Set_name("khar");
+    test.Update(t);
 
 }

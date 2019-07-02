f675d53c79465aa5521e3215e6ab8d565006635f
Mohamad Javad <heidarpoor2000@gmail.com>
2019-07-02 17:26:25 +0430

    Unit set and get value functions added

--- University-resource-management/organisation.cpp
+++ University-resource-management/organisation.cpp
@@ -1,1 +1,25 @@
 #include "organisation.h"
+void Unit::Set_cost(QString cost)
+{
+    this->cost = cost;
+}
+void Unit::Set_reqtype(QString req)
+{
+    this->reqtype = req;
+}
+void Unit::Set_name(QString name)
+{
+    this->name = name;
+}
+QString Unit::Get_cost()
+{
+    return cost;
+}
+QString Unit::Get_name()
+{
+    return name;
+}
+QString Unit::Get_reqtype()
+{
+    return reqtype;
+}

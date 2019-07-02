e69be12954141d168b186128f832d33976e5b699
habib <habib.sharif1379@gmail.com>
2019-07-02 17:25:00 +0430

    emplamentions added

--- University-resource-management/organisation.cpp
+++ University-resource-management/organisation.cpp
@@ -1,1 +1,19 @@
 #include "organisation.h"
+void organ::Set_ID(QString ID)
+{
+    this->ID=ID;
+}
+void organ::Set_name(QString name)
+{
+    this->name=name;
+}
+QString organ::Get_ID()
+{
+    return ID;
+}
+QString organ::Get_name()
+{
+    return name;
+}
+
+

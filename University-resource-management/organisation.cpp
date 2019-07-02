002827a2d601c272fb2806a24eaf2fb70442bddf
Mohamad Javad <heidarpoor2000@gmail.com>
2019-07-02 19:24:41 +0430

    encapsulation of office objects and unit objects done

--- University-resource-management/organisation.cpp
+++ University-resource-management/organisation.cpp
@@ -27,6 +27,18 @@ void office::Set_name(QString name)
 {
     this->name = name;
 }
+
+void office::Set_unit(int loc, Unit u)
+{
+    units[loc].Set_cost(u.Get_cost());
+    units[loc].Set_name(u.Get_name());
+    units[loc].Set_reqtype(u.Get_reqtype());
+}
+
+Unit office::Get_unit(int loc)
+{
+    return units[loc];
+}
 QString office::Get_name()
 {
     return name;
@@ -38,6 +50,19 @@ void organ::Set_ID(QString ID)
 void organ::Set_name(QString name)
 {
     this->name=name;
+}
+
+void organ::Set_office(int loc, office of)
+{
+    offices[loc] = of;
+    /*offices[loc].Set_name(of.Get_name());
+    offices[loc].Set_unit(of.Get_unit());*/
+    //if obj = obj not work use this code (a few bug )
+}
+
+office organ::Get_office(int loc)
+{
+    return offices[loc];
 }
 QString organ::Get_ID()
 {

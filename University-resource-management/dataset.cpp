6871a78eef00211bce016603a15d29e739a4b579
Mohammad <fathim2000astronomy@gmail.com>
2019-07-02 10:21:56 -0800

    modify dataset classes

--- University-resource-management/dataset.cpp
+++ University-resource-management/dataset.cpp
@@ -161,39 +161,51 @@ void User::Set_password(QString password ){
 void User::Set_imgurl(QString imgurl){
     this->imgurl=imgurl;
 }
+
 void User::Set_role(QString role){
     this->role=role;
 }
+
 void User::Set_address(QString add){
     this->address=add;
 }
+
 void User::Set_office_ID(QString office_ID){
     this->office_ID=office_ID;
 }
+
 QString User::Get_ID(){
     return ID;
 }
+
 QString User::Get_name(){
     return name;
 }
+
 QString User::Get_family(){
     return family;
 }
+
 QString User::Get_phonenum(){
     return phonenum;
 }
+
 QString User::Get_email(){
     return email;
 }
+
 QString User::Get_username(){
     return username;
 }
+
 QString User::Get_password(){
     return password;
 }
+
 QString User::Get_imgurl(){
     return imgurl;
 }
+
 QString User::Get_role(){
     return role;
 }

04960cc180eb601d26f5641437e08fc629ccaf19
mohammad <fathim2000astronomy@gmail.com>
2019-07-03 16:02:39 -0800

    select_office Implemented

--- University-resource-management/office_database.h
+++ University-resource-management/office_database.h
@@ -23,9 +23,10 @@ public:
     void Update(organ UTD);//change data in filevoid
     void show_Office_database();// just show data base.
     void show_column(QString col);//show a certain coumn.
+
     QString Select_obj(QString line ,int column);//return a column from a certain row
     QString Select_obj(int row ,int column);//return a column from a certain row
-    QString Select_office();
+    QString Select_office(QString organ,int column);
     QString Select_Unit();
 
     bool Search_ID(QString component);//find and return id of things that you search.

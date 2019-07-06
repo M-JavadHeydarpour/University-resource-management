c244e146ad786db6dbaef9c1f429b34f334c6760
mohammad <fathim2000astronomy@gmail.com>
2019-07-06 14:47:31 -0800

    try to connect sub bottn

--- University-resource-management/request.h
+++ University-resource-management/request.h
@@ -12,7 +12,6 @@ class content
 private:
     QString req_type;
 
-
 public:
     QString C_generate(int start_month,int start_day,int end_month,int end_day);
     QString S_generate(int month,int day,int S_code);
@@ -47,8 +46,9 @@ public:
     void Set_Unit_name(QString  name);
     void Set_type(QString type);
     void Set_time_in_use(QString time);
-    void Set_content();
+    void Set_content(QString content);
     void Set_ET_ID(QString ID);
+
     void Add_log(QString ET_ID , QString level);
 
     QString Get_Req_ID();
@@ -58,6 +58,8 @@ public:
     QString Get_Unit_name();
     QString Get_type();
     QString Get_time_in_use();
+
+    QString Get_content();
 
     QString Get_ET_ID();
 

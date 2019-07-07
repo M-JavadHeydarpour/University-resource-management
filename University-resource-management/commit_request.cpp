3f8b5cae106c87a6fe98538f024c553edf1e2a71
mohammad <fathim2000astronomy@gmail.com>
2019-07-07 12:28:43 -0800

    add user.txt

--- University-resource-management/commit_request.cpp
+++ University-resource-management/commit_request.cpp
@@ -26,10 +26,11 @@ commit_request::~commit_request()
 
 void commit_request::on_send_req_clicked()
 {
+    /*
     int start_month=ui->spinBox_month_from->value();
     int start_day=ui->spin_day_from->value();
     int end_month=ui->spinBox_month_to->value();
     int end_day=ui->spinBox_day_to->value();
-
+*/
 
 }

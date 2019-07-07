2def5c57ccfa35f1c6bd6d492b6707db31ece9cc
Mohamad Javad <heidarpoor2000@gmail.com>
2019-07-07 18:06:35 +0430

    NE

--- University-resource-management/userpanel.cpp
+++ University-resource-management/userpanel.cpp
@@ -11,10 +11,10 @@ userpanel::userpanel(QWidget *parent) :
     brain bll;
     User CU=bll.Load_user(bll.Get_ID());
     ui->setupUi(this);
-    ui->stackedWidget->setCurrentIndex(0);
+    /*ui->stackedWidget->setCurrentIndex(0);
     ui->label_3->setText(CU.Get_name());
     ui->label_2->setText(CU.Get_family());
-    ui->label->setText(CU.Get_username());
+    ui->label->setText(CU.Get_username());*/
 }
 
 userpanel::~userpanel()
